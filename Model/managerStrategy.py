af77f4d37ef3bce2741e29e0215bfab87bd7a082
Pungent Paws <whitney_mok@hotmail.com>
2019-01-24 10:17:29 +0100

    add StrategyA class to ManagerStrategy

--- Model/managerStrategy.py
+++ Model/managerStrategy.py
@@ -2,8 +2,7 @@ class ManagerStrategy(object):
 
 
     def __init__(self, model = None):
-        self.model = model
-
+        self.model = model 
 
     def getAssemblyStrategy(self, currentManager):
         raise NotImplementedError()
@@ -66,3 +65,37 @@ class EvenStrategy(ManagerStrategy):
     def getTradeStrategy(self, currentManager):
 
         pass
+
+class StrategyA(ManagerStrategy):
+
+    def getAssemblyStrategy(self, currentManager):
+        pass
+
+    def getTradeStrategy(self, currentManager):
+        n_players_to_fire = 0
+	    lower_boundary_wins = 0.3
+        middle_boundary_wins = 0.6
+        proportion_won_last_season = currentManager.game_history[-34:].count(1)/34      # 34 is always n_matches played last season?
+        if proportion_won_last_season < lower_boundary_wins: 
+	        n_players_to_fire = 9
+        if lower_boundary_wins <_  proportion_won_last_season  <_ middle_boundary_wins:
+	        n_players_to_fire = 5
+            #wait make sure that you don't randomly fire sb you just hired. \
+            # so just get the full list of ppl to replace
+        current_team_members = currentManager.team.keys()
+        players_to_fire = []
+        for i in range(n_players_to_fire):
+            players_to_fire.append(random.choice(current_team_members))# did I really access player objects by position keys?
+        for pos in players_to_fire:
+            pos.manager = None          
+            currentManager.team[pos] = None 
+            team_ranking_last_season = sorted(model.managers, key=lambda manager: manager.game_history[-34:]count(1), reverse=True)
+            for i-th in team_ranking_last_season:
+                i-th.managers..........
+
+                 
+
+
+        
+
+
